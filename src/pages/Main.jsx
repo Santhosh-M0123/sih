d2f2255ccbf23d5aee201b56d4d354f8a666cd89
Parameswaran <parameswaran8803@gmail.com>
2023-09-24 00:08:00 +0530

    another api added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,8 +5,110 @@ import Content from "../components/Content";
 import axios from "axios";
 const Main = () => {
   const [file, setFile] = useState(null);
-  const [variableData, setVariableData] = useState({})
   const path = "http://localhost:8000"
+
+  const variableData = { 
+      accessToken: "JjMVDrCnyOnXLWR",
+      images: "https://plant.id/media/imgs/a5bcbc8730e34b46b705b1ab9bda0ba7.jpg",
+      is_plant: {
+          binary: true,
+          threshold: 0.5,
+          probability: 1
+      },
+      suggestions: {
+          id: "c695f01775c8d5ac",
+          name: "Diplocarpon rosae",
+          probability: 0.9941538,
+          details: {
+              common_names: [
+                  "Rose Black Spot",
+                  "Black Rose Spot",
+                  "Black Spot"
+              ],
+              taxonomy: {
+                  class: "Leotiomycetes",
+                  genus: "Diplocarpon",
+                  order: "Helotiales",
+                  family: "Drepanopezizaceae",
+                  phylum: "Ascomycota",
+                  kingdom: "Fungi"
+              },
+              "url": "https://en.wikipedia.org/wiki/Diplocarpon_rosae",
+              "gbif_id": 2583566,
+              "inaturalist_id": 354898,
+              "rank": "species",
+              "description": {
+                  "value": "Diplocarpon rosae is a fungus that creates the rose black spot disease.\nBecause it was observed by people of various countries around the same time (around 1830), the nomenclature for the fungus varied with about 25 different names. The asexual stage is now known to be Marssonina rosae, while the sexual and most common stage is known as Diplocarpon rosae.\nDiplocarpon rosae grows over seasons as mycelia, ascospores, and conidia in infected leaves and canes. In the spring during moist, humid conditions, ascospores and conidia are wind-borne and rain-splashed to newly emerging leaf tissue.",
+                  "citation": "https://en.wikipedia.org/wiki/Diplocarpon_rosae",
+                  "license_name": "CC BY-SA 3.0",
+                  "license_url": "https://creativecommons.org/licenses/by-sa/3.0/"
+              },
+              "synonyms": [
+                  "Actinonema rosae",
+                  "Asteroma rosae",
+                  "Dicoccum rosae",
+                  "Dothidea rosae",
+                  "Fabraea rosae",
+                  "Marssonia rosae",
+                  "Marssonina rosae",
+                  "Phyllachora rosae"
+              ],
+              "image": {
+                  "value": "https://plant-id.ams3.cdn.digitaloceanspaces.com/knowledge_base/wikidata/5ab/5abfb0b47b537f7208806a052f7c2d03700e2994.jpg",
+                  "citation": null,
+                  "license_name": "CC0",
+                  "license_url": "https://creativecommons.org/publicdomain/zero/1.0/"
+              },
+              "edible_parts": null,
+              "language": "en",
+              "entity_id": "c695f01775c8d5ac"
+          }
+      },
+      "details": {
+          "common_names": [
+              "Rose Black Spot",
+              "Black Rose Spot",
+              "Black Spot"
+          ],
+          "taxonomy": {
+              "class": "Leotiomycetes",
+              "genus": "Diplocarpon",
+              "order": "Helotiales",
+              "family": "Drepanopezizaceae",
+              "phylum": "Ascomycota",
+              "kingdom": "Fungi"
+          },
+          "url": "https://en.wikipedia.org/wiki/Diplocarpon_rosae",
+          "gbif_id": 2583566,
+          "inaturalist_id": 354898,
+          "rank": "species",
+          "description": {
+              "value": "Diplocarpon rosae is a fungus that creates the rose black spot disease.\nBecause it was observed by people of various countries around the same time (around 1830), the nomenclature for the fungus varied with about 25 different names. The asexual stage is now known to be Marssonina rosae, while the sexual and most common stage is known as Diplocarpon rosae.\nDiplocarpon rosae grows over seasons as mycelia, ascospores, and conidia in infected leaves and canes. In the spring during moist, humid conditions, ascospores and conidia are wind-borne and rain-splashed to newly emerging leaf tissue.",
+              "citation": "https://en.wikipedia.org/wiki/Diplocarpon_rosae",
+              "license_name": "CC BY-SA 3.0",
+              "license_url": "https://creativecommons.org/licenses/by-sa/3.0/"
+          },
+          "synonyms": [
+              "Actinonema rosae",
+              "Asteroma rosae",
+              "Dicoccum rosae",
+              "Dothidea rosae",
+              "Fabraea rosae",
+              "Marssonia rosae",
+              "Marssonina rosae",
+              "Phyllachora rosae"
+          ],
+          "image": {
+              "value": "https://plant-id.ams3.cdn.digitaloceanspaces.com/knowledge_base/wikidata/5ab/5abfb0b47b537f7208806a052f7c2d03700e2994.jpg",
+              "citation": null,
+              "license_name": "CC0",
+              "license_url": "https://creativecommons.org/publicdomain/zero/1.0/"
+          },
+          "edible_parts": null,
+          "language": "en",
+          "entity_id": "c695f01775c8d5ac"
+  }
+  }
 
   const handleFileChange = async (e) => {
     // Get the selected file
@@ -26,7 +128,6 @@ const Main = () => {
       });
       
       // Handle the response from the backend as needed
-      setVariableData(response.data)
       console.log(response.data);
     } catch (error) {
       console.error(error);
